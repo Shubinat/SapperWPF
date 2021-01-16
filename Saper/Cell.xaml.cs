8c3dd7d29b8cac7c329ff4b9b6a7da3356b6d443
shubinat729@outlook.com <shubinat729@outlook.com>
2021-01-16 21:09:57 +0300

    Добавьте файлы проекта.

---
+++ Saper/Cell.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Saper
+{
+    /// <summary>
+    /// Клетка минного поля
+    /// </summary>
+    public partial class Cell : UserControl
+    {
+
+        /// <summary>
+        /// Карта к которой принадлежит данная клетка
+        /// </summary>
+        Map MainMap;
+
+        /// <summary>
+        /// Свойство, определяющее содержит ли клетка мину или нет
+        /// </summary>
+        public bool IsMine { get; set; }
+
+        /// <summary>
+        /// Координаты клетки
+        /// </summary>
+        public Position CellPosition { get; set; }
+
+        /// <summary>
+        /// Свойство, определяющее, открыта клетка или нет
+        /// </summary>
+        public bool Destroied { get; private set; } = false;
+
+        /// <summary>
+        /// Строка содержащая данные о содержании клетки, если "*" - клетка с миной
+        /// </summary>
+        public string CellStatus { get; set; } = "";
+
+        /// <summary>
+        /// Массив клеток находящихся рядом с данной
+        /// </summary>
+        public Cell[] CellsAround {
+            get
+            {
+                Cell[] Cells = {
+                MainMap.GetCell(new Position(CellPosition.x + 1, CellPosition.y)),
+                MainMap.GetCell(new Position(CellPosition.x + 1, CellPosition.y + 1)),
+                MainMap.GetCell(new Position(CellPosition.x, CellPosition.y + 1)),
+                MainMap.GetCell(new Position(CellPosition.x - 1, CellPosition.y + 1)),
+                MainMap.GetCell(new Position(CellPosition.x - 1, CellPosition.y)),
+                MainMap.GetCell(new Position(CellPosition.x - 1, CellPosition.y - 1)),
+                MainMap.GetCell(new Position(CellPosition.x, CellPosition.y - 1)),
+                MainMap.GetCell(new Position(CellPosition.x + 1, CellPosition.y - 1)),
+                };
+                return Cells;
+            }
+        }
+        
+        
+        public Cell(Map Parent, Position Position)
+        {
+            InitializeComponent();
+            CellPosition = Position;
+            this.MainMap = Parent;
+            
+            
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            if (MainMap.IsFirstClick)
+                MainMap.SpawnMines(this);
+            if (MainMap.IsWin == null) 
+                Destroy(this);
+            if(MainMap.DestroiedCount == MainMap.CellData.Length - MainMap.MinesCount)
+                MainMap.Win();  
+        }
+
+        /// <summary>
+        /// Метод вскрывающий клетку
+        /// </summary>
+        /// <param name="cell">Клетка для открытия</param>
+        private void Destroy(Cell cell)
+        {
+            switch (cell.CellStatus)
+            {
+                case "1":
+                    cell.CellText.Foreground = Brushes.Blue;
+                    break;
+                case "2":
+                    cell.CellText.Foreground = Brushes.Green;
+                    break;
+                case "3":
+                    cell.CellText.Foreground = Brushes.DarkRed;
+                    break;
+                case "4":
+                    cell.CellText.Foreground = Brushes.DarkBlue;
+                    break;
+                case "5":
+                    cell.CellText.Foreground = Brushes.Brown;
+                    break;
+                case "6":
+                    cell.CellText.Foreground = Brushes.DarkBlue;
+                    break;
+                case "7":
+                    cell.CellText.Foreground = Brushes.DarkGreen;
+                    break;
+                case "8":
+                    cell.CellText.Foreground = Brushes.Black;
+                    break;
+                case "*":
+                    cell.CellImg.Source = cell.ExplodedMineImg.Source;
+                    MainMap.GameOver();
+                    return;
+                default:
+                    break;
+            }
+            cell.CellGrid.Children.Remove(cell.CellButton);
+            MainMap.DestroiedCount++;
+            cell.Destroied = true;
+            if (cell.CellText.Text == "")
+            {
+                for(int i = 0; i < cell.CellsAround.Length; i++)
+                {
+                    if (cell.CellsAround[i] != null && !cell.CellsAround[i].Destroied)
+                    {
+                        Destroy(cell.CellsAround[i]);
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Класс содержащий позицию клетки x и y
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// X координата
+        /// </summary>
+        public int x { get; set; }
+        /// <summary>
+        /// Y координата
+        /// </summary>
+        public int y { get; set; }
+        public  Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Saper/Map.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Saper
+{
+    /// <summary>
+    /// Минное поле
+    /// </summary>
+    public partial class Map : UserControl
+    {
+        /// <summary>
+        /// Клетки содержащиеся в поле
+        /// </summary>
+        public Cell[,] CellData { get; set; }
+
+        /// <summary>
+        /// Количество мин в поле
+        /// </summary>
+        public int MinesCount { get; set; }
+
+        /// <summary>
+        /// Массив в котором содержатся клетки содержащие мины
+        /// </summary>
+        public Cell[] MinesCells { get; set; }
+        /// <summary>
+        /// Длинна поля по X
+        /// </summary>
+        public int XLength { get; set; } = 10;
+        /// <summary>
+        /// Длинна поля по Y
+        /// </summary>
+        public int YLength { get; set; } = 10;
+        /// <summary>
+        /// Поле определяющее был ли совершен первый клик или нет
+        /// </summary>
+        public bool IsFirstClick = true;
+
+        /// <summary>
+        /// Выйграл ли пользователь?   
+        /// null - идет игра.
+        /// true - победа.
+        /// false - поражение.
+        /// </summary>
+        public bool? IsWin { get; set; } = null;
+
+        /// <summary>
+        /// Количество открытых клеток
+        /// </summary>
+        public int DestroiedCount { get; set; } = 0;
+        public Map()
+        {
+            InitializeComponent();
+            GenerateMap();
+        }
+
+        /// <summary>
+        /// Создание колонок и заполнение клетками
+        /// </summary>
+        private void GenerateMap()
+        {
+            CellData = new Cell[XLength, YLength];
+            for (int y = 0; y < YLength; y++)
+                MapGrid.RowDefinitions.Add(new RowDefinition());
+            for (int x = 0; x < XLength; x++)
+                MapGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int y = 0; y < YLength; y++)
+            {
+                for (int x = 0; x < XLength; x++)
+                {
+                    var currCell = new Cell(this, new Position(x, y));
+                    MapGrid.Children.Add(currCell);
+                    Grid.SetRow(currCell, y);
+                    Grid.SetColumn(currCell, x);
+                    CellData[x, y] = currCell;
+                }
+            }
+        }
+        /// <summary>
+        /// Метод расставляющий мины
+        /// </summary>
+        /// <param name="ClickedCell">Первая клетка на которуб кликнул пользователь</param>
+        public void SpawnMines(Cell ClickedCell)
+        {
+            MinesCells = new Cell[MinesCount];
+            var rnd = new Random();
+            int counter = 0;
+            while (counter < MinesCount)
+            {
+                var currCell = CellData[rnd.Next(XLength), rnd.Next(YLength)];
+                if (currCell.IsMine == false && currCell != ClickedCell)
+                {
+                    currCell.CellImg.Visibility = Visibility.Visible;
+                    currCell.CellStatus = "*";
+                    currCell.IsMine = true;
+                    MinesCells[counter] = currCell;
+                    counter++;
+                }
+            }
+            IsFirstClick = false;
+
+            foreach (var currCell in CellData)
+            {
+                if (currCell.IsMine == false)
+                {
+                    int MinesAround = 0;
+                    foreach (var cell in currCell.CellsAround)
+                    {
+                        if (cell != null)
+                            if (cell.IsMine)
+                                MinesAround++;
+                    }
+                    if (MinesAround != 0)
+                    {
+                        currCell.CellText.Text = MinesAround.ToString();
+                        currCell.CellStatus = MinesAround.ToString();
+                    }
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Метод получающий клетку из общего массива принимая ее позицию.
+        /// </summary>
+        /// <param name="Position">Позиция клетки</param>
+        /// <returns>Клетка минного поля</returns>
+        public Cell GetCell(Position Position)
+        {
+            if ((Position.x >= 0 && Position.x < XLength) && (Position.y >= 0 && Position.y < YLength))
+                return CellData[Position.x, Position.y];
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Метод вскрывающий все мины и обозначающий поражение
+        /// </summary>
+        public void GameOver()
+        {
+            foreach (var Mine in MinesCells)
+            {
+                Mine.CellGrid.Children.Remove(Mine.CellButton);
+            }
+            IsWin = false;
+            var result = MessageBox.Show("Желаете повторить игру?", "Поражение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// Метод перезапускающий игру
+        /// </summary>
+        public void Restart()
+        {
+            MapGrid.Children.Clear();
+            MapGrid.ColumnDefinitions.Clear();
+            MapGrid.RowDefinitions.Clear();
+            CellData = null;
+            MinesCells = null;
+            IsFirstClick = true;
+            DestroiedCount = 0;
+            GenerateMap();
+            IsWin = null;
+
+        }
+
+        public void Win()
+        {
+            foreach (var Mine in MinesCells)
+            {
+                Mine.CellGrid.Children.Remove(Mine.CellButton);
+                Mine.CellImg.Source = Mine.DefusedMineImg.Source;
+            }
+            IsWin = true;
+            var result = MessageBox.Show("Желаете повторить игру?", "Победа", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Restart();
+            }
+        }
+    }
+}
